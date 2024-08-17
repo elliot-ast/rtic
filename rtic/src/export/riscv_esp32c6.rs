2f3e5b56e33cd7f5e2598890b30eb89b91189e1e
Elliot-ast <elliot@arcticspace.com>
2024-08-17 14:45:02 +0200

    testing things

--- rtic/src/export/riscv_esp32c6.rs
+++ rtic/src/export/riscv_esp32c6.rs
@@ -1,11 +1,12 @@
-use esp32c6::INTERRUPT_CORE0; //priority threshold control
+use esp32c6::INTERRUPT_CORE0; // Priority threshold control
 pub use esp32c6::{Interrupt, Peripherals, INTPRI};
 pub use riscv::asm::fence;
 pub use riscv::interrupt;
-pub use riscv::register::mcause; //low level interrupt enable/disable
-pub use riscv::register::mie; //low level interrupt enable/disable
+pub use riscv::register::mcause; // Low level interrupt enable/disable
+pub use riscv::register::mie; // Low level interrupt enable/disable
+
 #[cfg(all(feature = "riscv-esp32c6", not(feature = "riscv-esp32c6-backend")))]
-compile_error!("Building for the esp32c6, but 'riscv-esp32c6-backend not selected'");
+compile_error!("Building for the esp32c6, but 'riscv-esp32c6-backend' not selected");
 
 #[inline(always)]
 pub fn run<F>(priority: u8, f: F)
@@ -13,7 +14,7 @@ where
     F: FnOnce(),
 {
     if priority == 1 {
-        //if priority is 1, priority thresh should be 1
+        // If priority is 1, priority thresh should be 1
         f();
         unsafe {
             (*INTPRI::ptr())
@@ -21,7 +22,7 @@ where
                 .write(|w| w.cpu_int_thresh().bits(1));
         }
     } else {
-        //read current thresh
+        // Read current thresh
         let initial = unsafe {
             (*INTPRI::ptr())
                 .cpu_int_thresh()
@@ -30,16 +31,14 @@ where
                 .bits()
         };
         f();
-        //write back old thresh
+        // Write back old thresh
         unsafe {
-            riscv::asm::fence();
             (*INTPRI::ptr())
                 .cpu_int_thresh()
                 .write(|w| w.cpu_int_thresh().bits(initial));
         }
     }
 }
-
 /// Lock implementation using threshold and global Critical Section (CS)
 ///
 /// # Safety
@@ -58,11 +57,12 @@ where
 /// priority is current priority >= ceiling.
 #[inline(always)]
 pub unsafe fn lock<T, R>(ptr: *mut T, ceiling: u8, f: impl FnOnce(&mut T) -> R) -> R {
-    if ceiling == (15) {
-        //turn off interrupts completely, were at max prio
+    if ceiling == 15 {
+        // Turn off interrupts completely, we're at max priority
         let r = critical_section::with(|_| f(&mut *ptr));
         r
     } else {
+        // Read the current threshold
         let current = unsafe {
             (*INTPRI::ptr())
                 .cpu_int_thresh()
@@ -71,22 +71,27 @@ pub unsafe fn lock<T, R>(ptr: *mut T, ceiling: u8, f: impl FnOnce(&mut T) -> R) 
                 .bits()
         };
 
+        // Set the new threshold to ceiling + 1
         unsafe {
             (*INTPRI::ptr())
                 .cpu_int_thresh()
-                .write(|w| w.cpu_int_thresh().bits(ceiling + 1))
-        } //esp32c3 lets interrupts with prio equal to threshold through so we up it by one
-        let r = f(&mut *ptr);
-        unsafe {
-            (*INTPRI::ptr())
-                .cpu_int_thresh()
-                .write(|w| w.cpu_int_thresh().bits(current))
+                .write(|w| w.cpu_int_thresh().bits(ceiling + 1));
         }
+
+        // Execute the closure while the threshold is raised
+        let r = f(&mut *ptr);
+
+        // Restore the original threshold
+        unsafe {
+            (*INTPRI::ptr())
+                .cpu_int_thresh()
+                .write(|w| w.cpu_int_thresh().bits(current));
+        }
+
         r
     }
 }
 
-/// Sets the given software interrupt as pending
 #[inline(always)]
 pub fn pend(int: Interrupt) {
     unsafe {
@@ -113,7 +118,7 @@ pub fn pend(int: Interrupt) {
     }
 }
 
-// Sets the given software interrupt as not pending
+#[inline(always)]
 pub fn unpend(int: Interrupt) {
     unsafe {
         let peripherals = Peripherals::steal();
@@ -134,34 +139,37 @@ pub fn unpend(int: Interrupt) {
                 .INTPRI
                 .cpu_intr_from_cpu_3()
                 .write(|w| w.cpu_intr_from_cpu_3().bit(false)),
-            _ => panic!("Unsupported software interrupt"),
+            _ => panic!("Unsupported software interrupt"), //should never happen, checked at compile time
         }
     }
 }
 
 pub fn enable(int: Interrupt, prio: u8, cpu_int_id: u8) {
-    const INTERRUPT_MAP_BASE: u32 = 0x600c5000; //this isn't exposed properly in the PAC,
-                                                //should maybe figure out a workaround that
-                                                //doesnt involve raw pointers.
-                                                //Again, this is how they do it in the HAL
-                                                //but i'm really not a fan.
+    const INTERRUPT_MAP_BASE: *mut u32 =
+        unsafe { core::mem::transmute::<_, *mut u32>(INTERRUPT_CORE0::ptr()) };
+
     let interrupt_number = int as isize;
     let cpu_interrupt_number = cpu_int_id as isize;
 
     unsafe {
-        let intr_map_base = INTERRUPT_MAP_BASE as *mut u32;
+        let intr_map_base = INTERRUPT_MAP_BASE;
         intr_map_base
             .offset(interrupt_number)
             .write_volatile(cpu_interrupt_number as u32);
-        //map peripheral interrupt to CPU interrupt
+
         (*INTPRI::ptr())
             .cpu_int_enable()
-            .modify(|r, w| w.bits((1 << cpu_interrupt_number) | r.bits())); //enable the CPU interupt.
-        let intr = INTPRI::ptr();
-        let intr_prio_base = (*intr).cpu_intr_from_cpu_0().as_ptr();
+            .modify(|r, w| w.bits((1 << cpu_interrupt_number) | r.bits()));
 
-        intr_prio_base
-            .offset(cpu_interrupt_number)
-            .write_volatile(prio as u32);
+        (*INTPRI::ptr())
+            .cpu_int_pri(cpu_interrupt_number as usize)
+            .write(|w| w.bits(prio as u32));
+
+        (*INTPRI::ptr()).cpu_int_type().modify(|r, w| {
+            let interrupt_type = 1;
+            w.bits(
+                r.bits() & !(1 << cpu_interrupt_number) | (interrupt_type << cpu_interrupt_number),
+            )
+        });
     }
 }
